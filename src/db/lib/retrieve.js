7ed7702ce2977750c1d2f6f904d5744b118c8cf9
Paprikati <lisakarlincurtis@gmail.com>
2019-10-22 23:04:32 +0100

    handle retrieve errors correctly

--- src/db/lib/retrieve.js
+++ src/db/lib/retrieve.js
@@ -4,113 +4,124 @@ const async = require('async');
 
 // not in the same query set cause it needs to return something
 const retrieve = (db, config, callback) => {
-    const { tableId, columns, _filter } = config;
+    try {
+        const { tableId, columns, _filter } = config;
 
-    let tableConfig = db.tables[tableId];
+        let tableConfig = db.tables[tableId];
 
-    let columnsList = U.getColumnsList(columns, tableConfig.columns);
+        let columnsList = U.getColumnsList(columns, tableConfig.columns);
 
-    let filterString = U.getFilterString(_filter);
+        let filterString = U.getFilterString(_filter);
 
-    let mainQuery = format(
-        'SELECT %s from %I %s',
-        columnsList,
-        tableId,
-        filterString
-    );
+        let mainQuery = format(
+            'SELECT %s from %I %s',
+            columnsList,
+            tableId,
+            filterString
+        );
 
-    const mainQueryCB = (err, resp) => {
-        if (err) {
-            callback(err);
-        } else if (!resp || !resp.rows) {
-            callback([]);
-        } else {
-            let outputData = resp.rows;
-
-            // check if we need to fix the outputData at all
-            outputData = U.fixOutputData(outputData, tableConfig);
-
-            let relevantSubTables = [];
-
-            // check if we need any of the subcolumns of this table
-            if (tableConfig.subTables && !config.shallow) {
-                relevantSubTables = tableConfig.subTables.filter(
-                    subTable => !columns || columns.includes(subTable.id)
-                );
-            }
-
-            if (relevantSubTables.length === 0) {
-                if (config.index) {
-                    outputData = U.indexArr(outputData, tableConfig.key);
-                }
-                callback(undefined, outputData);
+        const mainQueryCB = (err, resp) => {
+            if (err) {
+                callback(err);
+            } else if (!resp || !resp.rows) {
+                callback([]);
             } else {
-                let idsToFind = outputData.map(x => x[tableConfig.key]);
+                let outputData = resp.rows;
 
-                const eachSubTableCallback = (subTable, c1) => {
-                    let subFilterString = format(
-                        '%s IN (%L)',
-                        subTable.parentid,
-                        idsToFind
+                // check if we need to fix the outputData at all
+                outputData = U.fixOutputData(outputData, tableConfig);
+
+                let relevantSubTables = [];
+
+                // check if we need any of the subcolumns of this table
+                if (tableConfig.subTables && !config.shallow) {
+                    relevantSubTables = tableConfig.subTables.filter(
+                        subTable => !columns || columns.includes(subTable.id)
                     );
-                    let retrieveColumns;
-                    if (config.subColumns && config.subColumns[subTable.id]) {
-                        retrieveColumns = config.subColumns[subTable.id];
-                        if (!retrieveColumns.includes(subTable.parentid)) {
-                            retrieveColumns.push(subTable.parentid);
-                        }
-                    }
+                }
 
-                    let retrieveOpts = {
-                        tableId: subTable.id,
-                        filter: subFilterString,
-                        columns: retrieveColumns,
-                        subColumns: config.subColumns
-                    }; // include subColumns incase you have nested subtables
-
-                    const retrieveCallback = (e, subRows) => {
-                        if (e) {
-                            c1(e);
-                        } else {
-                            if (subRows && subRows.length > 0) {
-                                // group subRows by key property
-                                var groupedRows = U.indexArr(
-                                    subRows,
-                                    subTable.parentid,
-                                    true
-                                );
-
-                                // add the data to 'outputData'
-                                outputData.forEach(row => {
-                                    let theseSubRows =
-                                        groupedRows[row[tableConfig.key]] || [];
-                                    row[subTable.id] = theseSubRows;
-                                });
-                            }
-                            c1();
-                        }
-                    };
-
-                    retrieve(db, retrieveOpts, retrieveCallback);
-                };
-
-                const finalCallback = () => {
+                if (relevantSubTables.length === 0) {
                     if (config.index) {
                         outputData = U.indexArr(outputData, tableConfig.key);
                     }
                     callback(undefined, outputData);
-                };
+                } else {
+                    let idsToFind = outputData.map(x => x[tableConfig.key]);
 
-                async.each(
-                    relevantSubTables,
-                    eachSubTableCallback,
-                    finalCallback
-                );
+                    const eachSubTableCallback = (subTable, c1) => {
+                        let subFilterString = format(
+                            '%s IN (%L)',
+                            subTable.parentid,
+                            idsToFind
+                        );
+                        let retrieveColumns;
+                        if (
+                            config.subColumns &&
+                            config.subColumns[subTable.id]
+                        ) {
+                            retrieveColumns = config.subColumns[subTable.id];
+                            if (!retrieveColumns.includes(subTable.parentid)) {
+                                retrieveColumns.push(subTable.parentid);
+                            }
+                        }
+
+                        let retrieveOpts = {
+                            tableId: subTable.id,
+                            filter: subFilterString,
+                            columns: retrieveColumns,
+                            subColumns: config.subColumns
+                        }; // include subColumns incase you have nested subtables
+
+                        const retrieveCallback = (e, subRows) => {
+                            if (e) {
+                                c1(e);
+                            } else {
+                                if (subRows && subRows.length > 0) {
+                                    // group subRows by key property
+                                    var groupedRows = U.indexArr(
+                                        subRows,
+                                        subTable.parentid,
+                                        true
+                                    );
+
+                                    // add the data to 'outputData'
+                                    outputData.forEach(row => {
+                                        let theseSubRows =
+                                            groupedRows[row[tableConfig.key]] ||
+                                            [];
+                                        row[subTable.id] = theseSubRows;
+                                    });
+                                }
+                                c1();
+                            }
+                        };
+
+                        retrieve(db, retrieveOpts, retrieveCallback);
+                    };
+
+                    const finalCallback = () => {
+                        if (config.index) {
+                            outputData = U.indexArr(
+                                outputData,
+                                tableConfig.key
+                            );
+                        }
+                        callback(undefined, outputData);
+                    };
+
+                    async.each(
+                        relevantSubTables,
+                        eachSubTableCallback,
+                        finalCallback
+                    );
+                }
             }
-        }
-    };
+        };
 
-    db.pool.query(mainQuery, mainQueryCB);
+        db.pool.query(mainQuery, mainQueryCB);
+    } catch (e) {
+        callback(e);
+    }
 };
 
 const retrieveById = (db, config, callback) => {
@@ -120,8 +131,11 @@ const retrieveById = (db, config, callback) => {
         if (err) {
             callback(err);
             return;
+        } else if (rows.length === 0) {
+            callback('MISSING_RESOURCE');
+        } else {
+            callback(null, rows[0]);
         }
-        callback(null, rows[0]);
     });
 };
 
